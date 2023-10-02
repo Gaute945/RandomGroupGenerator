180f5fcf36da4531cc9a1123f7e822b8986ff2e1
Gaute <mrboobai@proton.me>
2023-10-02 14:47:56 +0200

    Top half of app

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,312 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox9 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox10 = new TextBox();
+            textBox11 = new TextBox();
+            textBox12 = new TextBox();
+            textBox13 = new TextBox();
+            textBox14 = new TextBox();
+            textBox15 = new TextBox();
+            textBox16 = new TextBox();
+            textBox17 = new TextBox();
+            textBox18 = new TextBox();
+            textBox19 = new TextBox();
+            textBox20 = new TextBox();
+            textBox21 = new TextBox();
+            textBox22 = new TextBox();
+            textBox23 = new TextBox();
+            textBox24 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(31, 12);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(304, 27);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Group 1";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(31, 83);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(304, 27);
+            textBox2.TabIndex = 1;
+            textBox2.Text = "Group 3";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(471, 83);
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(304, 27);
+            textBox3.TabIndex = 3;
+            textBox3.Text = "Group 4";
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(471, 12);
+            textBox4.Name = "textBox4";
+            textBox4.ReadOnly = true;
+            textBox4.Size = new Size(304, 27);
+            textBox4.TabIndex = 2;
+            textBox4.Text = "Group 2";
+            textBox4.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(279, 45);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(56, 27);
+            textBox9.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(217, 45);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(56, 27);
+            textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(155, 45);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(56, 27);
+            textBox6.TabIndex = 10;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(93, 45);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(56, 27);
+            textBox7.TabIndex = 11;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(31, 45);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(56, 27);
+            textBox8.TabIndex = 12;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(31, 116);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(56, 27);
+            textBox10.TabIndex = 17;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(93, 116);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(56, 27);
+            textBox11.TabIndex = 16;
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(155, 116);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(56, 27);
+            textBox12.TabIndex = 15;
+            // 
+            // textBox13
+            // 
+            textBox13.Location = new Point(217, 116);
+            textBox13.Name = "textBox13";
+            textBox13.Size = new Size(56, 27);
+            textBox13.TabIndex = 14;
+            // 
+            // textBox14
+            // 
+            textBox14.Location = new Point(279, 116);
+            textBox14.Name = "textBox14";
+            textBox14.Size = new Size(56, 27);
+            textBox14.TabIndex = 13;
+            // 
+            // textBox15
+            // 
+            textBox15.Location = new Point(471, 45);
+            textBox15.Name = "textBox15";
+            textBox15.Size = new Size(56, 27);
+            textBox15.TabIndex = 22;
+            // 
+            // textBox16
+            // 
+            textBox16.Location = new Point(533, 45);
+            textBox16.Name = "textBox16";
+            textBox16.Size = new Size(56, 27);
+            textBox16.TabIndex = 21;
+            // 
+            // textBox17
+            // 
+            textBox17.Location = new Point(595, 45);
+            textBox17.Name = "textBox17";
+            textBox17.Size = new Size(56, 27);
+            textBox17.TabIndex = 20;
+            // 
+            // textBox18
+            // 
+            textBox18.Location = new Point(657, 45);
+            textBox18.Name = "textBox18";
+            textBox18.Size = new Size(56, 27);
+            textBox18.TabIndex = 19;
+            // 
+            // textBox19
+            // 
+            textBox19.Location = new Point(719, 45);
+            textBox19.Name = "textBox19";
+            textBox19.Size = new Size(56, 27);
+            textBox19.TabIndex = 18;
+            // 
+            // textBox20
+            // 
+            textBox20.Location = new Point(471, 116);
+            textBox20.Name = "textBox20";
+            textBox20.Size = new Size(56, 27);
+            textBox20.TabIndex = 27;
+            // 
+            // textBox21
+            // 
+            textBox21.Location = new Point(533, 116);
+            textBox21.Name = "textBox21";
+            textBox21.Size = new Size(56, 27);
+            textBox21.TabIndex = 26;
+            // 
+            // textBox22
+            // 
+            textBox22.Location = new Point(595, 116);
+            textBox22.Name = "textBox22";
+            textBox22.Size = new Size(56, 27);
+            textBox22.TabIndex = 25;
+            // 
+            // textBox23
+            // 
+            textBox23.Location = new Point(657, 116);
+            textBox23.Name = "textBox23";
+            textBox23.Size = new Size(56, 27);
+            textBox23.TabIndex = 24;
+            // 
+            // textBox24
+            // 
+            textBox24.Location = new Point(719, 116);
+            textBox24.Name = "textBox24";
+            textBox24.Size = new Size(56, 27);
+            textBox24.TabIndex = 23;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(346, 180);
+            button1.Name = "button1";
+            button1.Size = new Size(30, 29);
+            button1.TabIndex = 28;
+            button1.Text = "R";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(382, 180);
+            button2.Name = "button2";
+            button2.Size = new Size(30, 29);
+            button2.TabIndex = 29;
+            button2.Text = "E";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(418, 180);
+            button3.Name = "button3";
+            button3.Size = new Size(30, 29);
+            button3.TabIndex = 30;
+            button3.Text = "S";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(961, 689);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox20);
+            Controls.Add(textBox21);
+            Controls.Add(textBox22);
+            Controls.Add(textBox23);
+            Controls.Add(textBox24);
+            Controls.Add(textBox15);
+            Controls.Add(textBox16);
+            Controls.Add(textBox17);
+            Controls.Add(textBox18);
+            Controls.Add(textBox19);
+            Controls.Add(textBox10);
+            Controls.Add(textBox11);
+            Controls.Add(textBox12);
+            Controls.Add(textBox13);
+            Controls.Add(textBox14);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox9);
+            Controls.Add(textBox3);
+            Controls.Add(textBox4);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox9;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox10;
+        private TextBox textBox11;
+        private TextBox textBox12;
+        private TextBox textBox13;
+        private TextBox textBox14;
+        private TextBox textBox15;
+        private TextBox textBox16;
+        private TextBox textBox17;
+        private TextBox textBox18;
+        private TextBox textBox19;
+        private TextBox textBox20;
+        private TextBox textBox21;
+        private TextBox textBox22;
+        private TextBox textBox23;
+        private TextBox textBox24;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,20 @@ namespace RandomGroupGenerator
         {
             //test
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
