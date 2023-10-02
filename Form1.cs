36851e975c0afbf36df8922c044f6cef774dfced
Jensemannen <jensmar06@gmail.com>
2023-10-02 14:01:57 +0200

    added button

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace RandomGroupGenerator
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
